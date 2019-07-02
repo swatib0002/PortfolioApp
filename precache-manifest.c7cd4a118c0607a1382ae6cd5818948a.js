0640cb56915c00e220c55a620dbfbc9b7c3a4de7
Swati Bansal <swati.bansal@ET-C02VD4R5G8WN.local>
2019-07-02 18:34:07 +0530

    Updates

---
+++ precache-manifest.c7cd4a118c0607a1382ae6cd5818948a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac89ef854fcd1d9aca40f07a479593e6",
+    "url": "/PortfolioApp/index.html"
+  },
+  {
+    "revision": "69cf4432735c46c33efc",
+    "url": "/PortfolioApp/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "75beda23446be2154bf3",
+    "url": "/PortfolioApp/static/css/main.179a90e4.chunk.css"
+  },
+  {
+    "revision": "69cf4432735c46c33efc",
+    "url": "/PortfolioApp/static/js/2.9e30f44f.chunk.js"
+  },
+  {
+    "revision": "75beda23446be2154bf3",
+    "url": "/PortfolioApp/static/js/main.eee64818.chunk.js"
+  },
+  {
+    "revision": "1b5033fe105c05ef6ce6",
+    "url": "/PortfolioApp/static/js/runtime~main.09c0c2aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PortfolioApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
